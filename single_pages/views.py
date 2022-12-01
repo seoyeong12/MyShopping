bbc7e720f29ac46d541b624648151aaeaacdbb50
username <email>
2022-12-01 20:27:35 +0900

    자기소개,회사소개 페이지 기능 추가

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from item.models import Item
+from item.models import Item,Comment
 def landing(request):
     recent_items = Item.objects.order_by('-pk')[:3]
     return render(
@@ -10,8 +10,13 @@ def landing(request):
         }
     )
 def about_me(request):
+    items = Item.objects.order_by('-pk')
+    comments = Comment.objects.order_by('pk')
     if request.user.is_authenticated:
-        return render(request, 'single_pages/about_me.html')
+        return render(request, 'single_pages/about_me.html',{
+            'items' : items,
+            'comments' : comments
+        })
     else:
         return redirect('/item/')
 def about_us(request):
